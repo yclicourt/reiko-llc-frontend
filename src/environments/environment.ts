ae8dfac1fcf173a67b1b2053882c02b64ded6870
Yoadev <yclicourt@gmail.com>
2026-03-08 19:10:24 -0400

    fix: Cambio en la URl de la edge functions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   RESEND_API_KEY: 're_HeSRAMH6_HYahngzXoyi2zonF19wWAFJU',
   EDGE_FUNCTIONS_URL:
-    'https://dpezgmyapqspnatzwdmo.supabase.co/functions/v1/resend-email',
+    'https://klubumnlatyirgfkmefr.supabase.co/functions/v1/resend-email',
   SUPABASE_ANON_KEY:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwZXpnbXlhcHFzcG5hdHp3ZG1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5NDA0MzYsImV4cCI6MjA4NjUxNjQzNn0.xQ-hj7IBcUYyeoxv0ToCU6g6LQezEq6CEe0LxtGXr-Y',
 };
