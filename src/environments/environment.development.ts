d2b51dd499a11566f2f602253d4256f7654e4d06
Yoadev <yclicourt@gmail.com>
2026-03-08 19:41:58 -0400

    Modificacion de la URL de la edge functions

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   RESEND_API_KEY: "re_37NhL8GG_L7uGwLiet9jt3FUqhpkrnDt3",
-  EDGE_FUNCTIONS_URL:"https://klubumnlatyirgfkmefr.supabase.co/functions/v1/send-mail"
+  EDGE_FUNCTIONS_URL:"https://qkfcosnluhldisbislut.supabase.co/functions/v1/send-email"
   
 };
